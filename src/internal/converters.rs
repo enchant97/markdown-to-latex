98870577404fc1c80154f6301f7e06d59dff1654
Leo Spratt <enchant97@users.noreply.github.com>
2022-01-14 20:27:13 +0000

    allow for both std::io and files to be used

--- src/internal/converters.rs
+++ src/internal/converters.rs
@@ -117,13 +117,19 @@ pub fn get_latex_doc_start() -> String {
     return "\\maketitle\\tableofcontents\\newpage".to_string();
 }
 
-pub fn process(src_path: &str, dst_path: &str) {
-    // TODO handle errors better than using 'unwrap'
+/// Setup a two streams from given filenames
+pub fn setup_file_streams(src_path: &str, dst_path: &str) -> (BufReader<File>, BufWriter<File>) {
     let src_file = File::open(src_path).unwrap();
     let src_stream = BufReader::new(src_file);
     let dst_file = File::create(dst_path).unwrap();
-    let mut dst_stream = BufWriter::new(dst_file);
+    let dst_stream = BufWriter::new(dst_file);
+    return (src_stream, dst_stream);
+}
 
+/// Process the source (markdown file) and
+/// output converted data the destination source
+pub fn process<S: BufRead, D: Write>(src_stream: S, dst_stream: &mut D) {
+    // TODO handle errors better than using 'unwrap'
     let mut meta_buffer = String::new();
     let mut at_start = true;
     let mut in_header = false;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io;
+use std::io::{BufReader, BufWriter};
 use structopt::StructOpt;
 
 pub mod internal;
@@ -6,22 +9,49 @@ use internal::converters;
 
 #[derive(StructOpt)]
 struct CliArgs {
-    #[structopt(parse(from_os_str))]
-    input_path: std::path::PathBuf,
-    #[structopt(parse(from_os_str))]
-    output_path: std::path::PathBuf,
+    #[structopt(
+        parse(from_os_str),
+        short = "f",
+        help = "Read from a file instead from stdin"
+    )]
+    input_path: Option<std::path::PathBuf>,
+    #[structopt(
+        parse(from_os_str),
+        short = "o",
+        help = "Write to a file instead of stdout"
+    )]
+    output_path: Option<std::path::PathBuf>,
+}
+
+fn open_src_file(path: &str) -> BufReader<File> {
+    let src_file = File::open(path).unwrap();
+    let src_stream = BufReader::new(src_file);
+    return src_stream;
+}
+
+fn open_dst_file(path: &str) -> BufWriter<File> {
+    let dst_file = File::create(path).unwrap();
+    let dst_stream = BufWriter::new(dst_file);
+    return dst_stream;
 }
 
 fn main() {
+    // TODO handle errors better than using 'unwrap'
     let args = CliArgs::from_args();
 
-    println!(
-        "input={:?}, output={:?}",
-        &args.input_path, &args.output_path
-    );
+    let src_stream: Box<dyn io::BufRead> = match args.input_path {
+        None => Box::new(BufReader::new(io::stdin())),
+        Some(file_name) => Box::new(open_src_file(
+            file_name.into_os_string().into_string().unwrap().as_str(),
+        )),
+    };
 
-    converters::process(
-        args.input_path.into_os_string().into_string().unwrap().as_str(),
-        args.output_path.into_os_string().into_string().unwrap().as_str()
-    );
+    let mut dst_stream: Box<dyn io::Write> = match args.output_path {
+        None => Box::new(BufWriter::new(io::stdout())),
+        Some(file_name) => Box::new(open_dst_file(
+            file_name.into_os_string().into_string().unwrap().as_str(),
+        )),
+    };
+
+    converters::process(src_stream, &mut dst_stream);
 }
